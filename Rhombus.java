e97c93cbac8d5fbec0432681c4ec7d5a79d060c7
Mostofa Ahmed <34137210+MdMostofaAhmed@users.noreply.github.com>
2017-11-30 23:59:07 +0600

    Add files via upload

---
+++ Rhombus.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Rhombus {
+	public static void main(String args [])
+	{
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter an Integer: ");
+		int n=scan.nextInt();
+		
+		for (int i=1; i<=n; i++)
+		{
+			for (int j=i; j<n; j++)
+			{
+				System.out.print("");
+			}
+			for (int k=1; k)
+		}
+	}
+
+}
